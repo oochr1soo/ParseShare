9dcd85c93ba36fb4a7cfa536b7cc058ff20e7a0a
Chris Clough <cclough@jltm41.jaalam.net>
2015-07-09 16:33:25 -0400

    Added Parse and created login screen

--- ParseShare/AppDelegate.swift
+++ ParseShare/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.enableLocalDatastore()
+        
+        Parse.setApplicationId("0blFy7SRDxg0PXg7OM7t2KK4Mz7USCXbH6TF5DSM", clientKey: "zIHE6R0PDxIg5La76qV3urQbDJ7Kg9oAh615PeBu")
+        
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
         return true
     }
 
---
+++ ParseShare/LoginViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  ParseShare
+//
+//  Created by Chris Clough on 7/9/15.
+//  Copyright (c) 2015 Chris Clough. All rights reserved.
+//
+
+import UIKit
+import Parse
+import Bolts
+
+class LoginViewController: UIViewController {
+    
+    // Outlets
+    
+    @IBOutlet weak var emailAddressTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    // Properties
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println("Hello World!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func loginButtonPressed(sender: UIButton) {
+        // Log user in
+        // Verify credentials against Parse
+    }
+
+    @IBAction func signUpButtonPressed(sender: UIButton) {
+        // Segue to Signup Form
+    }
+}
+
