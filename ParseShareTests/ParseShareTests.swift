a58975bcc55b4b6705411b3e51105a4e64d1fe32
Chris Clough <cclough@jltm41.jaalam.net>
2015-07-09 09:54:31 -0400

    Initial Commit

---
+++ ParseShareTests/ParseShareTests.swift
@@ -0,0 +1,36 @@
+//
+//  ParseShareTests.swift
+//  ParseShareTests
+//
+//  Created by Chris Clough on 7/9/15.
+//  Copyright (c) 2015 Chris Clough. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ParseShareTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
